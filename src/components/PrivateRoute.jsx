a41d7210d4bd2d9ee46d6f7f18c124aff13c7c8f
Anand Undavia <abundavia@gmail.com>
2019-06-09 15:49:02 +0530

    READY

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,45 +2,19 @@ import React from "react";
 import { Route, Redirect } from "react-router";
 
 import { store } from "../reducers";
-import firebase from "../config/firebase-config";
 
 import logger from "../utils/logger";
 const log = logger("private-route");
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
 	log.debug(`Incoming route: ${rest.path}`);
+	//  <Route {...rest} component={Component} />;
 	switch (rest.path) {
-		case "/wait-for-call":
-		case "/make-call": {
-			if (firebase.auth().currentUser) {
-				log.debug(`User exists. allowed access to '${rest.path}'`);
-				return <Route {...rest} component={Component} />;
-			} else {
-				log.warn("Did not find user in firebase.auth().currentUser");
-			}
-			break;
-		}
-		case "/ongoing-call": {
-			if (firebase.auth().currentUser) {
-				const state = store.getState();
-				const { callee, caller } = state.call;
-				if (callee && Object.keys(callee) !== 0 && callee.accepted) {
-					return <Route {...rest} component={Component} />;
-				} else if (caller && Object.keys(caller) !== 0) {
-					return <Route {...rest} component={Component} />;
-				} else {
-					log.warn("state.call.callee either empty or falsy");
-				}
-			} else {
-				log.warn("Did not find user in firebase.auth().currentUser");
-			}
-			break;
-		}
 		default: {
-			log.debug("no rules specified. redirecting to /registration");
+			log.debug("no rules specified. redirecting to /index");
 		}
 	}
-	return <Redirect to="/registration" />;
+	return <Redirect to="/index" />;
 };
 
 export default PrivateRoute;
