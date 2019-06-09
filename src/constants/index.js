d1fbc4353440d2fea248192e680975f485d58be1
Anand Undavia <abundavia@gmail.com>
2019-06-09 20:50:10 +0530

    Firebase host

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,9 @@
 export default {
 	api: {
-		base: "http://localhost:8000/api",
+		base:
+			process.env.REACT_APP_ENV === "production"
+				? "https://code-analyzer-api.herokuapp.com/api"
+				: "http://localhost:8000/api",
 		endpoints: {
 			file: "/v1/analyze/file",
 			raw: "/v1/analyze/raw"
